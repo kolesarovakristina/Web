39c4fd25d187823dde405ad42ec3394e70daa166
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-01-16 11:57:40 +0100

    add new table

--- Web/formular.js
+++ Web/formular.js
@@ -85,8 +85,34 @@
                 person.gender = $("input[name='gender']:checked").val();
                 console.log(person);
                 persons.push(person);
+                printTable();
             };
             data();
+
+            function printTable(){
+                if(persons.length>0){
+                    var divTable=$('.table');
+                    var table=$("<table/>");
+                    var line=$("<tr/>");
+                    var col1=$("<th/>");
+                    $(col1).append('First name');
+                    var col2=$("<th/>");
+                    $(col2).append('Last name');
+                    var col3=$("<th/>");
+                    $(col3).append('Date of birth');
+                    var col4=$("<th/>");
+                    $(col4).append('Gender');
+                    var col5=$("<th/>");
+                    $(col5).append('Delete');
+                    line.append(col1);
+                    line.append(col3);
+                    line.append(col3);
+                    line.append(col4);
+                    line.append(col5);
+                    table.append(line);
+                    divTable.append(table);
+                }
+            }
         }
         error = false;
 
