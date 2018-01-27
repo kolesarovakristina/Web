3155a823b14631bcdff99bd9e589e0986c3ba765
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-01-27 22:24:55 +0100

    added google maps and design

--- Web/weather.js
+++ Web/weather.js
@@ -15,75 +15,64 @@ $(document).ready(function() {
                 url: urllink,
                 data : { format: 'json' },  
             error : function(){
-             //vypis chyby ak zlyha komunikacia so serverom
+             //vypis chyby 
                 $('.mainTable').html("Error! Invalid request!");
            },
             dataType: 'json',
             success : function(data){
                 
                 //dynamic table
-                $('.mainTable').empty();
-                var table=$('<table/>');
-                var tr=getLine('City:', city);
-                table.append(tr);
-                $('.mainTable').append(table);
-                
-                var tr=getLine('Country:', $('#code').val());
-                table.append(tr);
-                
-                var tr=getLine('Temperature:', parseFloat(data.main.temp-273.15).toFixed(1)+" ℃");
-                table.append(tr);
-                
-                var tr=getLine('Humidity:', data.main.humidity+" %");
-                table.append(tr);
-                
-                var tr=getLine('Description:', data.weather[0].description);
-                table.append(tr);
-                
-                var tr=getLine('Pressure:', data.main.pressure+' hPa');
-                table.append(tr);
-                
-                if($("#details").is(":checked") == true){
+      $(".mainTable").empty();
+    var table = $('<table/>')
+    table.addClass('table  table-bordered table-hover ');
+    $(".mainTable").append(table);
+
+    
+
+    table.append("<th class='col-md-12 headRow text-center'colspan ='2'>Weather in "+data.name+"</th>");
+    table.append(getRow('Temperature: ',parseFloat(data.main.temp-273.15).toFixed(1) + '°','Humidity: ',data.main.humidity+'%' ));
+    table.append(getRow('Desc: ', data.weather[0].description,'Pressure: ',data.main.pressure + ' hPa' ));
+
+    if($('#details').is(":checked")==true){
+
+        table.append(getRow('Wind speed: ', data.wind.speed + 'm/s','Visibility:', parseFloat(data.visibility/1000).toFixed(2)+" km" ));
+        table.append(getRow('Min temperature: ', parseFloat(data.main.temp_min-273.15).toFixed(1)+ '°','Max temperature: ',parseFloat(data.main.temp_max-273.15).toFixed(1)+ '°' ));
+        var sunrise=data.sys.sunrise;
+                    var sunriseTime=new Date(1000*sunrise);
+                    hR = sunriseTime.getHours();
+                    mR = sunriseTime.getMinutes(); 
+                         
                     
-                    
-                    var tr=getLine('Sunrise:', data.sys.sunrise);
-                    table.append(tr);
-                    
-                    var tr=getLine('Sunset:', data.sys.sunset);
-                    table.append(tr);
-                    
-                    var tr=getLine('Wind:', data.wind.speed+' m/s');
-                    table.append(tr); 
-                    
-                    var tr=getLine('Min temperature:', parseFloat(data.main.temp_min-273.15).toFixed(1)+" ℃");
-                    table.append(tr);
-                    
-                    var tr=getLine('Max temperature:', parseFloat(data.main.temp_max-273.15).toFixed(1)+" ℃");
-                    table.append(tr);
-                    
-                    var tr=getLine('Visibility:', data.visibility);
-                    table.append(tr);
-                };
+        var sunset=data.sys.sunset;
+                    var sunsetTime=new Date(1000*sunset);
+                    hS = sunsetTime.getHours(); 
+                    mS = sunsetTime.getMinutes(); 
+            table.append(getRow('Sunrise:', hR+":"+mR, 'Sunset:', hS+":"+mS));         
+                   
+
+      var mapsUrl = "<a href =\"http://www.google.com/maps/search/?api=1&query="+data.coord.lat+','+ data.coord.lon+"\" target=\"_blank\">Google maps</a>";
+      console.log(mapsUrl);
+      table.append(getRow(mapsUrl,'','',''));           
+    };
                 
             },
             type: 'GET'
            }); //AJAX
            
-       } //if city.length riadok 5
+       } //if city.length 
        
    }); // CLICK
+
+function getRow(name1,data1,name2,data2){
+    var row = $('<tr id="tr"/>');
+    var column = $('<td class="td1"/>');
+    var column2 = $('<td class="td2"/>');
+    row.append(column);
+    column.html(name1 + data1);
+    row.append(column2);
+    column2.html(name2 + data2);
+    return row;
     
-    function getLine(data1,data2){
-        var tr=$('<tr/>');
-        var td1=$('.<td/>');
-        $(td1).append(data1);
-        var td2=$('<td/>');
-        $(td2).append(data2);
-        tr.append(td1);
-        tr.append(td2); 
-        
-        return tr;
-    };
+ };
     
 }); // READY
-
